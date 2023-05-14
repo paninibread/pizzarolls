a92dc51c3e0fbe0a7f756e6848d27a9870d47e02
Prashast Sharma <prashastsharma48@gmail.com>
2023-05-13 23:00:24 -0400

    WIP basic input
    basic movement w/ rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 10f;
+    public float rot = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,35 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * Time.deltaTime * transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += speed * Time.deltaTime * -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * Time.deltaTime * transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += speed * Time.deltaTime * -transform.right;
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0,-rot,0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, rot, 0) * Time.deltaTime);
+        }
     }
 }
