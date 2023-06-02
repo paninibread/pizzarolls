3e8ee109a228ce7fbf15314ba0b38599f16171d5
Prashast Sharma <prashastsharma48@gmail.com>
2023-06-01 20:35:31 -0600

    Repo health check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
-    public float rot = 65f;
-
-    public float G = 9.8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,36 +14,5 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
-        transform.position += G * Time.deltaTime * -transform.up;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += speed * Time.deltaTime * transform.forward;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += speed * Time.deltaTime * -transform.forward;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += speed * Time.deltaTime * transform.right;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += speed * Time.deltaTime * -transform.right;
-        }
-
-        if(Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(new Vector3(0,-rot,0) * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(new Vector3(0, rot, 0) * Time.deltaTime);
-        }
     }
 }
